513750f02ec8aeba35a19593868bfe4736c7f725
pablo0601 <39735574+pablo0601@users.noreply.github.com>
2018-05-29 09:58:05 -0400

    Initial commit

---
+++ src/solitare/Card.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solitare;
+
+
+public class Card {
+    public enum Suit{
+        Spade,
+        Club,
+        Heart,
+        Diamond
+    }
+    
+    public enum Value{
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+    
+    public void SetSuit(Suit s){
+    
+    }
+    
+    public void SetValue(Value v){
+        
+    }
+    
+    
+}
