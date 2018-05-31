ae82325c27302dca6f7a20f2bf418231530893bd
pablo0601 <39735574+pablo0601@users.noreply.github.com>
2018-05-31 10:31:14 -0400

    Shuffle

--- src/solitare/Deck.java
+++ src/solitare/Deck.java
@@ -7,12 +7,13 @@ package solitare;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 /**
  *
  * @author michael.pierik700
  */
 public class Deck {
+    
     Card[] c = new Card[52];
     public Deck() {
         int i = 0;
@@ -24,6 +25,18 @@ public class Deck {
         }
     }
  
+    public void Shuffle(){
+        Random rand = new Random();
+        for(int i = 0; i < 51; i++){
+            Card memory = null;
+            int j = rand.nextInt((51 - i) + 1) + i;
+            memory = this.c[i];
+            this.c[i] = this.c[j];
+            this.c[j] = memory;
+        }
+        
+        
+    }
 //    Card AofS = new Card(Card.Suit.Spade, Card.Value.Ace);
 //    Card TwoofS = new Card(Card.Suit.Spade, Card.Value.Two);
 //    Card ThreeofS = new Card(Card.Suit.Spade, Card.Value.Three);
